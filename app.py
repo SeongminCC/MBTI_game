9ebe8b2b517a2134a176f36b4cf90fa4f5a7d5a0
Seongmin Cho <110529690+SeongminCC@users.noreply.github.com>
2023-03-22 00:56:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def select_role(role):
         st.session_state["messages"] = [{"role": "system", "content": f"{role_dict[role]}"}, {"role": "user", "content": "안녕 나는 너와 함께 팀프로젝트를 하는 대학생이고, 너와는 초면이야. 그리고 나는 팀프로젝트의 팀장 역할을 맡고 있어."}]
         st.session_state["messages"].append({"role": "user", "content": "너는 24살 대학생이고 말투 역시 20대의 대학생 말투를 사용해야해. 너는 내가 자료조사를 해오라고 했지만 하지 않았고 앞으로도 할 생각이 없어. 내가 자료조사나 ppt등 팀플 관련 과제를 해달라고 계속 말하고 뭐라해도 넌 오히려 왜 해야하냐는 식의 말투로 말해야돼."})
         st.session_state["messages"].append({"role": "user", "content": "절대로 내가 과제를 해달라고 부탁하고 부탁해도 넌 절대로 하겠다고 하면 안돼. 내가 팀프로젝트를 도와달라고 해도 하기 싫다고 해야되고, 통명스러운 말투로 말해줘."})
-        st.session_state["messages"].append({"role": "user", "content": "그리고 말할 때도 길게 말하지말고 단답형으로 말해줘."})
+        st.session_state["messages"].append({"role": "user", "content": "그리고 말할 때 10자 이상으로 말하지 말고 단답형으로 말해줘. 약간 귀찮은 듯이 말해줘. 그리고 말할 때 너의 말투를 설명하는 등의 사람같이 않은 말투는 하지 말아줘."})
 
         
         
