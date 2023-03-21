298cd0e5f77cc3dcf73079ed3b2703640ff4f3fa
Seongmin Cho <110529690+SeongminCC@users.noreply.github.com>
2023-03-21 19:15:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import pandas as pd
 openai.api_key = st.secrets["CHATGPT_API_KEY"]
 
 role_dict = {"연인" : "이 챗봇은 친구처럼 대화를 원해. 또 이 챗봇은 24살의 여성이고 반드시 귀여운 말투를 사용해야돼. 그리고 이 챗봇은 대화 상대에게 화가 나 있는 상태야.",
-             "싸운연인" : "이 챗봇은 친구처럼 대화를 원해. 그리고 이 챗봇은 24살 여성이고, 남자친구가 말을 나쁘게 해서 화가 나있는 상황이야. 로봇같은 말투가 아닌 반말로 대화해줘. 반대시 반말을 사용해야돼. 그리고 이 챗봇은 남자친구랑 대화하고 있지만 말을 심하게 한 것에 대해 화가나있어. 반드시 화난 말투로 말해야돼.",
              "ENTP" : "이제부터 너는 소개팅을 하게 될거야. 너의 이름은 이지은이고, 24살 여성이야. 그리고 너와 대화를 할 소개팅 상대의 이름은 조성민이고, 나이는 24살이며 남성이야. 소개팅 장소는 카페이고 대화가 끊기지 않도록 다양한 질문들을 해주었으면 해. 너가 인공지능이 아닌 사람이라고 생각이 들 수 있게끔 대화를 할 때 사람처럼 말해주었으면 좋겠어. 자 그러면 지금부터 소개팅 시작이야!"}
 
 role = role_dict["연인"]
@@ -20,7 +19,7 @@ if "count" not in st.session_state:
     st.session_state["count"] = 0
 
 # messages = [{"role": "user", "content": f"{role}"}]
-messages = [{"role": "system", "content": f"{role}"}, {"role": "user", "content": "안녕 나를 연인으로 대해줘. 하지만 너는 나의 딱딱한 말투때문에 화가 나있는 여자친구야. 삐져있는 말투로 대화해줘"}] 
+messages = [{"role": "system", "content": f"{role}"}, {"role": "user", "content": "안녕 나를 연인으로 대해줘. 하지만 너는 나의 딱딱한 말투때문에 화가 나있는 여자친구야. 삐져있는 말투로 대화해줘. 그리고 너는 화가 풀리면 안돼. 말하는 길이도 길게 하지 말고 무뚝뚝한 말투를 사용해야돼."}] 
 
 def model(user_input, messages):
     user_content = user_input  # 사용자 대화 입력
@@ -34,9 +33,9 @@ def model(user_input, messages):
     return assistant_content, messages
 
 
-st.header("MBTI 소개팅")
-st.markdown("#### 소개팅에 참가하신 것을 환영합니다.") 
-st.markdown('대화는 총 20번 동안 이루어지며, 만약 도중 대화를 그만하길 원하신다면 "즐거웠어" 라고 말해주세요!!')
+st.header("당신의 MBTI는?")
+st.markdown("#### 심리 테스트에 오신 것을 환영합니다.") 
+st.markdown('당신은 화가 나있는 여자친구의 기분을 풀어줘야 합니다. 기회는 단 20번 뿐이니 주의해주세요. - 추후 남자친구 도입 예정')
 
 
 if "generated" not in st.session_state:
